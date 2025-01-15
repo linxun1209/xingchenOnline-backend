9223dceda158c02c977c16c986c9f377afc3b041
diaoxingchen@mail.qzkeji.cn <diaoxingchen@mail.qzkeji.cn>
2025-01-15 10:07:18 +0800

    整合网关gateway

---
+++ XingchenXue-gateway/src/main/java/com/xingchen/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.xingchen.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 35238
+ * 功能: 网关
+ * 时间: 2024-04-14 21:50
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
